665fb0e9429dbb3a9255402ed7d85c81e97e068b
dimsparagis0210 <dimsparagis@gmail.com>
2023-11-05 19:20:14 +0200

    Restaged files

---
+++ src/store/index.jsx
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { inputSlice } from "./input-slice";
+
+const store = configureStore({
+    reducer: {
+        input: inputSlice.reducer, 
+    }
+});
+
+export default store;
