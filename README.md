3dcaa2cecc9f08710035d2f71346f5a4feadc13a
dimsparagis0210 <dimsparagis@gmail.com>
2023-11-06 17:39:39 +0200

    Fixed not typing bug

--- README.md
+++
@@ -1,6 +0,0 @@
-# AeppApp
-Run instrictions:
-1) Install or git pull the files
-2) cd in your directory
-3) npm install
-4) npm run dev
--- src/components/UI/Header.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Header.module.css';
-
-export const Header = () => {
-    return (
-        <div className={styles.header}>
-            <h1>Template Design </h1>
-        </div>
-    )
-}
