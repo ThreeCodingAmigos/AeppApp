6c8a7eea1adc37278926d7382aab3cff9bcdc6f2
dimsparagis0210 <dimsparagis@gmail.com>
2023-11-09 18:40:18 +0200

    Fixed backspace and focusedRow number bugs

--- src/components/Input/InputContainer.jsx
+++ src/components/Input/InputContainer.jsx
@@ -6,26 +6,6 @@ import { inputActions } from "../../store/input-slice";
 
 export const InputContainer = () => {
   const rows = useSelector((state) => state.input.rows);
-  const focusedRow = rows.find((row) => row.isFocused);
-  const focusedInputRef = useRef(null);
-  const dispatch = useDispatch();
-
-  const changeHandler = (event) => {
-    console.log(event.target.value);
-    if (event.key === "Enter") {
-      dispatch(inputActions.enter(event.code));
-    } 
-    else if (event.key === 'Backspace' && focusedRow.rowContent === '' && focusedRow.rowNumber !== 0) {
-      console.log(focusedRow.rowContent);
-      dispatch(inputActions.deleteRow());
-    }
-    console.log('Change Handler',);
-    dispatch(inputActions.type(event.target.value));
-  };
-
-  useEffect(() => {
-    focusedInputRef.current.focus();
-  }, [focusedRow]);
 
   return (
     <div className={styles.inputContainer}>
@@ -33,10 +13,8 @@ export const InputContainer = () => {
         <Row
           rowNumber={row.rowNumber}
           rowContent={row.rowContent}
-          onChange={(ev) => changeHandler(ev)}
           key={row.key}
           placeholder={row.placeholder}
-          ref={row.isFocused ? focusedInputRef : null}
           isFocused={row.isFocused}
         />
       ))}
--- src/components/Input/Row.jsx
+++ src/components/Input/Row.jsx
@@ -1,18 +1,57 @@
 import styles from "./Row.module.css";
-import React, { forwardRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useRef, useEffect } from "react";
+import { inputActions } from "../../store/input-slice";
 
-export const Row = forwardRef((props, ref) => {
-    const rowClasses = props.isFocused ? `${styles.row} + ' ' + ${styles.focusedRow}` : styles.row;
+export const Row = (props) => {
+  const rows = useSelector((state) => state.input.rows);
+  const focusedRow = rows.find((row) => row.isFocused);
+  const dispatch = useDispatch();
+  const focusedInputRef = useRef(null);
 
-    return (
-        <div className={rowClasses}>
-            <p>{props.rowNumber}</p>
-            <input
-                type="text"
-                onKeyDown={props.onChange}
-                placeholder={props.placeholder}
-                ref={ref}
-            />
-        </div>
-    );
-});
+  const rowClasses = props.isFocused
+    ? `${styles.row} + ' ' + ${styles.focusedRow}`
+    : styles.row
+  ;
+
+  const clickHandler = (event) => {
+    dispatch(inputActions.clickToChangeRow());
+  }
+  const changeHandler = (event) => {
+    if (event.key === "Enter") {
+      dispatch(inputActions.enter(event.code));
+      console.log('Enter');
+    } else if (event.key === "Backspace") {
+      if (focusedRow.rowContent === '' && focusedRow.rowNumber !== 0) {
+        event.preventDefault(); //For not removing the last character, when moving to the previous line
+        dispatch(inputActions.deleteRow(focusedRow.rowNumber));
+        console.log('Delete Row');
+      }
+      if (focusedRow.rowContent !== '') {
+        console.log('Backspace');
+        const newContent = Array.from(event.target.value);
+        newContent.pop();
+        console.log(newContent);
+        dispatch(inputActions.backspace(newContent.toString()));
+      }
+    } else {
+      dispatch(inputActions.type(event.key));
+    }
+  };
+
+  useEffect(() => {
+    focusedInputRef.current.focus();
+  }, [focusedRow]);
+
+  return (
+    <div className={rowClasses}>
+      <p>{props.rowNumber}</p>
+      <input
+        type="text"
+        onKeyDown={changeHandler}
+        placeholder={props.placeholder}
+        ref={focusedInputRef}
+      />
+    </div>
+  );
+};
