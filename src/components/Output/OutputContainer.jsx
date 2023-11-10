070a7acc8df3bac707050538d681b3a077190ca8
dimsparagis0210 <dimsparagis@gmail.com>
2023-11-10 14:07:09 +0200

    Added run button(Without functionality)

--- src/components/Output/OutputContainer.jsx
+++ src/components/Output/OutputContainer.jsx
@@ -1,10 +1,13 @@
 import styles from './OutputContainer.module.css';
 import { useState } from 'react';
-
+import { Button } from '../Input/Button';
 export const OutputContainer = () => {
     return (
         <div className={styles.container}>
-            <p>Output</p>
+            <section className={styles.header}>
+                <p>Output</p>
+                <Button />
+            </section>
             <hr/>
         </div>
     );
